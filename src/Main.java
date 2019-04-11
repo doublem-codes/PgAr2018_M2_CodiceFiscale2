c22c0e91801c5c078a7045e6762470882f4ea30c
doublem-codes <m.milanesi004@studenti.unibs.it>
2019-04-11 16:35:00 +0200

    v2.0

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,7 @@ public class Main {
         Element rootPerson = lettore.read("inputPersone.txt", "persona", true);
         Element rootComuni = lettore.read("comuni.txt", "comune", true);
         Element rootCodiciFiscali = lettore.read("codiciFiscali.txt", " ", false);
-        //Element trips = lettore.read("trips.txt");
 
-        //trips.printOnConsole();
-        //rootPerson.printOnConsole();
 
         System.out.println("");
 
