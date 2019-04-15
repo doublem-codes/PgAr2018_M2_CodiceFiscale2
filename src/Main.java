41ef0bd5e40c9cdeb4752e5db895908af7ecb3d6
EdoGitMira <e.mirandola@studenti.unibs.it>
2019-04-15 16:00:50 +0200

    prova

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-import java.lang.System;
 public class Main {
 
     public static void main(String[] args) {
 
         Reader lettore = new Reader();
-
+        Systemcode systemcode= new Systemcode();
+        Element rootCommon = lettore.read("comuni.txt", "comune", true);
+        Element rootCodeFiscal = lettore.read("codiciFiscali.txt", " ", false);
         Element rootPerson = lettore.read("inputPersone.txt", "persona", true);
-        Element rootComuni = lettore.read("comuni.txt", "comune", true);
-        Element rootCodiciFiscali = lettore.read("codiciFiscali.txt", " ", false);
 
-        rootPerson.transfer();
-        System.out.println("");
+        systemcode.arrayListCommon = rootCommon.transferCommon();
+        systemcode.rightFiscalCode = rootCodeFiscal.transferCode(systemcode.arrayListCommon);
+        systemcode.arrayListPerson = rootPerson.transferPerson();
+
+
+        systemcode.fiscalCodePerson();
+        systemcode.writeXmlOutput("codePerson.xml","utf-8");
 
     }
-
 }
--- src/Person.java
+++ src/Person.java
@@ -3,15 +3,13 @@ public class Person {
     private String firstName ;
     private String lastName ;
     private String sex ;
-
     private String common;
+    private String fiscalCode;
     private int day;
     private int month;
     private int year;
-    private String fiscalCode;
 
-
-    public void Person(String firstName, String lastName, String sex, String common, Date date) {
+    public void setPerson(String firstName, String lastName, String sex, String common, Date date) {
         this.firstName = firstName.toUpperCase();
         this.lastName = lastName.toUpperCase();
         this.sex = sex;
@@ -20,25 +18,18 @@ public class Person {
         this.month = date.getMounth();
         this.year = date.getYear();
     }
-    
-    public void Person(String firstName, String lastName, String sex, String common, int day, int month, int year) {
-        this.firstName = firstName.toUpperCase();
-        this.lastName = lastName.toUpperCase();
-        this.sex = sex;
-        this.common = common;
-        this.day = day;
-        this.month = month;
-        this.year = year;
-    }
 
     public String getFiscalCode() {
         return fiscalCode;
     }
 
-
     public String getFirstName() {
 
         return firstName;
+    }
+
+    public void setFiscalCode(String code){
+        this.fiscalCode =code;
     }
 
     public String getLastName() {
